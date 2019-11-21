98feca5061a97fef1bea7003de19b2981e5b96b3
Juna Arnold <fuccinspace@gmail.com>
2019-11-21 14:39:00 -0700

    final word count function

--- Week12-utility.py
+++ Week12-utility.py
@@ -15,3 +15,19 @@ def LoadFile(file_name):
         contents.append(line.strip())
     return PrintOutput(contents)
 
+def UpdateString(str_one, str_two, index):
+    list_one = []
+    for i in str_one:
+        list_one.append(i)
+    list_one[index] = str_two
+    output = ''
+    for j in list_one:
+        output += j
+    return PrintOutput(output)
+
+def FinalWordCount(list_thing, string):
+    occur = 0
+    for i in list_thing:
+        if i.find(string) > -1:
+            occur += 1
+    return PrintOutput(occur)
