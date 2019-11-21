3fe0da626ad3cd79a9c2b34bb5e6d481cc73b17e
Juna Arnold <fuccinspace@gmail.com>
2019-11-21 14:29:54 -0700

    loadfile function

--- Week12-utility.py
+++ Week12-utility.py
@@ -7,3 +7,10 @@ def PrintOutput(text):
     print("OUTPUT", text)
     return
 
+def LoadFile(file_name):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    contents = []
+    for line in lines:
+        contents.append(line.strip())
+    return PrintOutput(contents)
